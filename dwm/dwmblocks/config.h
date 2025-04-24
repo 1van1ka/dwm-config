2a5d6131bba8bf008fcfdcac88872dca65e39286
Ivan <you@example.com>
2025-04-24 20:09:46 +0300

    script for display new packets

--- dwm/dwmblocks/config.h
+++ dwm/dwmblocks/config.h
@@ -5,25 +5,19 @@
 #define DELIMITER ""
 
 #define MAX_BLOCK_OUTPUT_LENGTH 45
-
-// Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
-
-// Control whether a leading delimiter should be prepended to the status.
+#define CLICKABLE_BLOCKS 0
 #define LEADING_DELIMITER 0
-
-// Control whether a trailing delimiter should be appended to the status.
 #define TRAILING_DELIMITER 0
 
-// Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/layoutkb", 1, 0)\ 
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/layoutkb", 0, 10)\
     X("",  "$HOME/.config/dwm/dwmblocks/scripts/audio", 1, 1)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/brightness", 0, 1)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/memory",1, 0)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/cpu",1, 0)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/battery",3, 0)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/notify",3, 1)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/datetime",10, 0)\
-    X("",  "$HOME/.config/dwm/dwmblocks/scripts/network",1, 0) 
-#endif  // CONFIG_H
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/brightness", 0, 2)\
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/memory",1 , 0)\
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/cpu", 1, 4)\
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/battery", 3, 0)\
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/notify", 3, 3)\
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/datetime", 10, 0)\
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/packets", 1, 0)\
+    X("",  "$HOME/.config/dwm/dwmblocks/scripts/network", 600, 0) 
+#endif
